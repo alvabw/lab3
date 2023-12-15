76295dc6eb129c9e33264ab83e391a60fb9e2703
Juliaw803e <41002169+Juliaw803e@users.noreply.github.com>
2023-12-15 11:08:02 +0100

    Fredag förmiddag 9-11

--- code/exerc3.cpp
+++ code/exerc3.cpp
@@ -5,6 +5,34 @@
  *********************************************/
 
 #include <iostream>
+#include <cmath>
+#include <format>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+
+ double calculate(double k) {
+        double const factor{ 8.0 * k };
+        double const init{ 4.0 / (factor + 1.0) - 1.0 / (factor + 4.0) };
+
+        std::vector<double> other_factors{ 4.0, 5.0, 6.0 };
+
+        //räknar lite?
+        std::transform(other_factors.begin(), other_factors.end(), other_factors.begin(),
+            [factor](double value) {return -1.0 / (factor + value); }); 
+
+        //Summera ihop
+        double result{ init };
+
+       result = std::accumulate(other_factors.begin(), other_factors.end(), 0.0); 
+
+        return result;
+    }
+
+
+
+
 
 int main() {
     std::cout << "Enter number of terms: ";
@@ -12,4 +40,29 @@ int main() {
     std::cin >> n;
 
     // ADD CODE
+
+    //Iota för varje element för den ++
+    std::vector<double> terms(n);
+    std::iota(terms.begin(), terms.end(), 0.0);
+
+    std::vector<double> power_terms{ terms };
+
+    //ändrar nu terms 
+    //skicka till calculte
+    std::transform(terms.begin(), terms.end(), terms.begin(),
+        [](double term) {return calculate(term); });
+
+    //Fixar lilla perentesen
+    std::transform(power_terms.begin(), power_terms.end(), power_terms.begin(),
+        [](double term) {return std::pow(16.0, -term); });
+
+    double result{ 0.0 };
+
+    //inner product av terms(stora parentesen) och powwer_terms(lilla perentesen)
+    result = std::inner_product(terms.begin(), terms.end(), power_terms.begin(), 0.0,
+        std::plus<double>(), [](double termsValue, double powerValue) 
+        { return  termsValue * powerValue; });
+
+    std::cout << std::format("{:.15f}\n", result);
+    
 }
